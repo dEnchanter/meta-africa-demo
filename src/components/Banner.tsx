4bf5e49c37a273ec92ce44052ec226e8c343051e
dEnchanter <afolabiopaleye@gmail.com>
2024-03-05 13:08:42 +0100

    ..

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -26,7 +26,7 @@ const Banner = () => {
         >
           <div 
             className='relative aspect-[1155/700] w-full
-            bg-gradient-to-br from-black to-gray-800 opacity-60'
+            bg-gradient-to-br from-black to-gray-800 opacity-90 lg:opacity-60'
           />
         </div>
 
